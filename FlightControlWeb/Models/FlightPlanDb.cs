218b479527050b09974ae665e9d7dc8259d77186
Meshi Biton <meshi.biton@live.biu.ac.il>
2020-05-25 19:54:36 +0300

    add class we will use her for db

---
+++ FlightControlWeb/Models/FlightPlanDb.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightControlWeb.Models
+{
+    public class FlightPlanDb
+    {
+        private FlightPlan flightPlan;
+        private string id;
+
+        public FlightPlanDb(FlightPlan flightPlan,string id)
+        {
+            this.flightPlan = flightPlan;
+            this.id = id;
+        }
+
+        public string GetId()
+        {
+            return this.id;
+        }
+        public FlightPlan GetFlightPlan()
+        {
+            return this.flightPlan;
+        }
+    }
+}
