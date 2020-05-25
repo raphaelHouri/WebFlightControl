27cf7adc212e7a52d3dbbf79f2c87cafc75c23e5
raphaelHouri <raphaelhouri789@gmail.com>
2020-05-25 18:13:41 +0300

    database qurey

--- FlightControlWeb/SQLCommands.cs
+++ FlightControlWeb/SQLCommands.cs
@@ -50,11 +50,11 @@ namespace FlightControlWeb
             databaseObject.CloseConnection();
 
             Console.WriteLine("Rows Added : {0}", result);
-            string userName = Console.ReadLine();
+ 
         }
         public void addListSegmet(FlightPlan flightPlan, Database databaseObject, string id)
         {
-            int length = flightPlan.Segments.Length;
+            int length = flightPlan.Segments.Count;
             for (int i=0;i<length;i++ )
             {
                 string query = "INSERT INTO Segments ('id', 'serial','longitude', 'latitude','timespan') VALUES (@id, @serial, @longitude, @latitude, @timespan );";
@@ -66,7 +66,7 @@ namespace FlightControlWeb
                 myCommand.Parameters.AddWithValue("@serial", i);
                 myCommand.Parameters.AddWithValue("@longitude", flightPlan.Segments[i].Longitude);
                 myCommand.Parameters.AddWithValue("@latitude", flightPlan.Segments[i].Latitde);
-                myCommand.Parameters.AddWithValue("@timespan", flightPlan.Segments[i].TimespanSeconds);
+                myCommand.Parameters.AddWithValue("@timespan", flightPlan.Segments[i].Timespan_Seconds);
                 int result = myCommand.ExecuteNonQuery();
                 if (result > 0)
                 {
@@ -110,6 +110,59 @@ namespace FlightControlWeb
             }
             databaseObject.CloseConnection();
 
+        }      
+        public void returnJsonPlan(string id, Database databaseObject)
+        {
+            string query = $"DELETE FROM Flight WHERE id = '{id}';";
+            SQLiteCommand myCommand = new SQLiteCommand(query, databaseObject.myConnection);
+            databaseObject.OpenConnection();
+            int result = myCommand.ExecuteNonQuery();
+            if (result > 0)
+            {
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine(result);
+            }
+            databaseObject.CloseConnection();
+            query = $"DELETE FROM Segments WHERE id = '{id}';";
+            myCommand = new SQLiteCommand(query, databaseObject.myConnection);
+            databaseObject.OpenConnection();
+            result = myCommand.ExecuteNonQuery();
+            if (result > 0)
+            {
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine(result);
+            }
+            databaseObject.CloseConnection();
+
+        }  
+        public List<Coordinate> segmentList(string id, Database databaseObject)
+        {
+
+            // SELECT FROM DATABASE
+            string query = "SELECT * FROM Segmaents  WHERE id = 1 ORDER BY serial ASC;";
+            SQLiteCommand myCommand = new SQLiteCommand(query, databaseObject.myConnection);
+            databaseObject.OpenConnection();
+            SQLiteDataReader result = myCommand.ExecuteReader();
+            // Creating a List of coordinate 
+            List<Coordinate> segmentList = new List<Coordinate>();
+            if (result.HasRows)
+            {
+                while (result.Read())
+                {
+
+                    segmentList.Add(new Coordinate( Convert.ToDouble($"{result["longitude"]}"), Convert.ToDouble($"{result["latitude"]}")));
+                    
+                }
+            }
+            databaseObject.CloseConnection();
+            return segmentList;
+
         }
 
     
@@ -119,20 +172,25 @@ namespace FlightControlWeb
             RandomGenerator generator = new RandomGenerator();
             return generator.RandomPassword();
         }
-        public Coordinate getEndCoors(Segment[] seg)
+        public Coordinate getEndCoors(List<Segment> seg)
         {
-            int lastSegIndex = seg.Length - 1;
+            int lastSegIndex = seg.Count - 1;
             Coordinate coord = new Coordinate(seg[lastSegIndex].Latitde, seg[lastSegIndex].Latitde);
             return coord;
         }
-        public DateTime getEndTime(Segment[] seg, DateTime startTime)
+        public DateTime getEndTime(List<Segment>seg, DateTime startTime)
         {
-            int sumSeconds = 0;
-            for(int i=0 ; i < seg.Length; i++)
+            double sumSeconds = 0;
+            foreach (Segment item in seg)
             {
-                sumSeconds += seg[i].TimespanSeconds;
+                sumSeconds += item.Timespan_Seconds;
             }
             return startTime.AddSeconds(sumSeconds);
+            //for (int i=0 ; i < seg.Length; i++)
+            //{
+            //    sumSeconds += seg[i].Timespan_Seconds;
+            //}
+            //return startTime.AddSeconds(sumSeconds);
 
         }
 
